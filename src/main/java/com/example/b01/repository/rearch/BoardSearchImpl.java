cb9c52a5bd46fc8c8467c1a01b70d00412444194
demo5 <skyup112@naver.com>
2025-05-26 15:34:14 +0900

    20250526

--- src/main/java/com/example/b01/repository/rearch/BoardSearchImpl.java
+++ src/main/java/com/example/b01/repository/rearch/BoardSearchImpl.java
@@ -65,6 +65,7 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
         }
         query.where(board.bno.gt(0L));
 
+        //paging
         this.getQuerydsl().applyPagination(pageable, query);
         List<Board> list = query.fetch();
         long count = query.fetchCount();
